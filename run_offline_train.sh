e2349cc6339276ef73c433b4a23c5f56d397e524
Zhiyuan Chen <chenzhiyuan.sjtu.edu.cn>
2025-11-12 14:31:00 +0800

    训练集量加大

--- run_offline_train.sh
+++ run_offline_train.sh
@@ -2,4 +2,6 @@ python3 diffusion/train.py \
     --mode offline \
     --name offline_exp \
     --group my_offline_tests \
+    --dataset_paths BoB_012.pickle BoB_45.pickle BoB_67.pickle \
+    --buffer_size 4000000 \
     --use_wandb
